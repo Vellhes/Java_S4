31d89f8c93918dca05343fed1644006cd4cc8599
Malo <Malo@DESKTOP-56FOIPE.home>
2021-02-18 13:46:59 +0100

    Création du package Controller ainsi que de la classe MatchesController pour le jeu d'allumettes.

---
+++ src/games/controller/MatchesController.java
@@ -0,0 +1,31 @@
+package games.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class MatchesController {
+
+	 @FXML
+	    private AnchorPane pane_player;
+
+	    @FXML
+	    private Button btn_one;
+
+	    @FXML
+	    private Button btn_two;
+
+	    @FXML
+	    private Label lbl_nb;
+
+	    @FXML
+	    private Label lbl_recap;
+
+	    @FXML
+	    private AnchorPane pane_cpu;
+
+	    @FXML
+	    private Label lbl_cpu;
+	
+}
