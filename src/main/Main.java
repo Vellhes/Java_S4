e39e2f6c4128fa562cfa22841c598f1d3f6f6cdc
Malo <Malo@DESKTOP-56FOIPE.home>
2021-02-19 09:00:10 +0100

    Avancée sur le jeu des allumettes

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,10 @@
 package main;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+public class Main {
 
-
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-
-		Parent root = FXMLLoader.load(getClass().getResource("../games/fxml/Matches.fxml"));
-		primaryStage.setTitle("Pulls moches");
-		primaryStage.setScene(new Scene(root,673,282));
-		primaryStage.show();
-	}
-	
 	public static void main(String[] args) {
-		launch(args);
+		// TODO Auto-generated method stub
+
 	}
+
 }
